83e34b5f37ed3904bf8f3e701207282868654f79
Tracy Reuther <tracyreuther@gmail.com>
2019-04-27 23:58:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  let countdown = 4;
+  while (countdown > 0) {
+    console.log(facts + "!!!");
+  }
+}

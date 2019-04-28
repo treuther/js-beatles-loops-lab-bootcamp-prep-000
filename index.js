3bd74cf464b39e1e174587810a968cab2775e21e
Tracy Reuther <tracyreuther@gmail.com>
2019-04-28 00:20:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function theBeatlesPlay(musicians, instruments) {
 function iLoveTheBeatles(num) {
   var array = [];
   do {
-    array.push(console.log("I love the Beatles!"));
+    array.push("I love the Beatles!");
     num++;
   } while (num < 15);
   return array;

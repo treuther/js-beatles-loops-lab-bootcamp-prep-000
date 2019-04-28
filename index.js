5a9a5069d88f5726f91d8c3a81fe41911747409e
Tracy Reuther <tracyreuther@gmail.com>
2019-04-28 00:07:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
  function johnLennonFacts(facts) {
-  var results[];
+  var results = [];
   var count = 0;
   while (count < facts.length) {
     results.push(facts[count] + "!!!");
